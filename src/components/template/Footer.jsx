ee680815636494430f0411faa21ea93bc5c690f7
chagasleandro <69199409+chagasleandro@users.noreply.github.com>
2021-11-03 22:14:35 -0300

    Add files via upload

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Curso da <strong> Cod<span className='text-danger'>3</span>r </strong>
+            adaptado com <i className="fa fa-heart text-danger"></i> por
+            <strong> Leandro Chagas</strong>
+        </span>
+    </footer>
