3b4e8345338ed46f2b512c8cfe7ac15832ddd71a
chagasleandro <69199409+chagasleandro@users.noreply.github.com>
2021-11-07 22:46:43 -0300

    Delete src directory

--- src/components/template/NavItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-export default props =>
-    <Link to={props.path}>
-        <i className={`fa fa-${props.icon}`}></i> {props.name}
-    </Link>
